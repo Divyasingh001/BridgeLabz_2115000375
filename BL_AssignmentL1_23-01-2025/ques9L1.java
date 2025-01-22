da70512630e8062bc6f669dd2f958c8ff775b3cb
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-22 23:52:05 +0530

    Countdown for rocket using forloop

---
+++ BL_AssignmentL1_23-01-2025/ques9L1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ques9L1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the starting value for the countdown: ");
+        int counter = scanner.nextInt();
+        for (int i = counter; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Liftoff! 🚀");
+        
+        scanner.close();
+    }
+}
