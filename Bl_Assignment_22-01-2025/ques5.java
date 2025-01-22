c959a7fcfe2e257b2cd33ca72b36783bd44a3e89
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-22 11:55:13 +0530

    Pen distribution

---
+++ Bl_Assignment_22-01-2025/ques5.java
@@ -0,0 +1,10 @@
+public class ques5{
+    public static void main(String[] args) {
+        int totalPens = 14;
+        int students = 3;
+        int pensPerStudent = totalPens / students;
+        int remainingPens = totalPens % students;
+        System.out.println(
+                "Pen Per Student is " + pensPerStudent + " and remaining pen not distributed is " + remainingPens);
+    }
+}
