e1c21d88e9e3e8051e4e751ab69ec4565201e7c0
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-23 14:03:24 +0530

    multiple below 100

---
+++ BL_AssignmentL2_23-01-2025/ques12L2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ques12L2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("The multiples of " + number + " below 100 are:");
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.println(i); 
+                }
+            }
+        } else {
+            System.out.println("Please enter a positive number.");
+        }
+        scanner.close();
+    }
+}
