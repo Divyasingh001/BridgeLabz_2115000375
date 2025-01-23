6e26ff7f6e59f8066ebf5b8538a55c41022ffdb3
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-23 14:25:52 +0530

    Amstrong number

---
+++ BL_AssignmentL3_23-01-2025/ques1L3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        int sum = 0; 
+        int originalNumber = number; 
+        while (originalNumber != 0) {
+            int remainder = originalNumber % 10; 
+            sum += Math.pow(remainder, 3); 
+            originalNumber /= 10; 
+        }
+        if (sum == number) {
+            System.out.println(number + " is an Amstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+        scanner.close();
+    }
+}
