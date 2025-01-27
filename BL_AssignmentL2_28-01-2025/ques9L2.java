1c0ad0c64f9485c3c9d63801aa33557d4e2a7f21
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-27 16:42:57 +0530

    even odd

---
+++ BL_AssignmentL2_28-01-2025/ques9L2.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class ques9L2 {
+
+    public static boolean isPositive(int number) {
+        return number >= 0;
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+   
+    public static int compare(int number1, int number2) {
+        if (number1 > number2) {
+            return 1; 
+        } else if (number1 == number2) {
+            return 0; 
+        } else {
+            return -1; 
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (isPositive(numbers[i])) {
+                System.out.print("Number " + numbers[i] + " is positive and ");
+                if (isEven(numbers[i])) {
+                    System.out.println("even.");
+                } else {
+                    System.out.println("odd.");
+                }
+            } else {
+                System.out.println("Number " + numbers[i] + " is negative.");
+            }
+        }
+
+        int comparisonResult = compare(numbers[0], numbers[numbers.length - 1]);
+        System.out.print("Comparison of the first and last elements: ");
+        if (comparisonResult == 1) {
+            System.out.println("The first element is greater than the last element.");
+        } else if (comparisonResult == 0) {
+            System.out.println("The first element is equal to the last element.");
+        } else {
+            System.out.println("The first element is less than the last element.");
+        }
+
+        scanner.close();
+    }
+}
