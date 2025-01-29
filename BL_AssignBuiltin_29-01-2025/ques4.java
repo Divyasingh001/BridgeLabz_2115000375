f6510e602c045482013268bcf7402dbc7dab4ec6
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-29 13:57:45 +0530

    fibonacci number

---
+++ BL_AssignBuiltin_29-01-2025/ques4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ques4 {
+    public static void main(String[] args) {
+        int terms = getInput("Enter the number of terms: ");
+        generateFibonacci(terms);
+    }
+
+    public static int getInput(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
+    public static void generateFibonacci(int n) {
+        int first = 0, second = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
