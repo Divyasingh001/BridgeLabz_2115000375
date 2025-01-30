b4e8314dbf29ae71345406f7f8c8f0e3abfc2b8b
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-30 14:12:29 +0530

    Mostfrequentcharacter

--- BL_AssignString_29-01-2025/ques10.java
+++ BL_AssignString_29-01-2025/ques10.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
-public class ques10 {
+public class RemoveSpecificCharacter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String str = scanner.nextLine();
-        System.out.print("Enter the character to remove: ");
+        System.out.print("Enter character to remove: ");
         char charToRemove = scanner.next().charAt(0);
-        String modifiedStr = str.replaceAll(String.valueOf(charToRemove), "");
-        System.out.println("Modified String: " + modifiedStr);
+        System.out.println("Modified string: " + removeCharacter(str, charToRemove));
+    }
+
+    public static String removeCharacter(String str, char charToRemove) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != charToRemove) {
+                result += str.charAt(i);
+            }
+        }
+        return result;
     }
 }
