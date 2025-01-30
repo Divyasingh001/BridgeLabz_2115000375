acece7a3026e11d27c8f1658639177ff82a7d4a2
Divyasingh001 <divyashunk2003@gmail.com>
2025-01-30 13:39:24 +0530

    Reverse string

---
+++ BL_AssignString_29-01-2025/ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ques2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        char[] chars = input.toCharArray();
+        int left = 0, right = chars.length - 1;
+        
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        String reversed = new String(chars);
+        System.out.println("Reversed string: " + reversed);
+    }
+}
