ccecc14879327640497da4e67595e66b95c847e1
Divyasingh001 <divyashunk2003@gmail.com>
2025-02-24 14:30:09 +0530

    Added

---
+++ BL_Assignment_24_02_2025/Bl_AssignmentAnnotations/ArrayListCheck.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ArrayListCheck{
+	 @SuppressWarnings("unchecked")
+	public static void main(String args[]){
+		List arr=new ArrayList();
+		arr.add("Divya");
+		arr.add(22);
+		System.out.println(arr);
+		
+	}
+}
