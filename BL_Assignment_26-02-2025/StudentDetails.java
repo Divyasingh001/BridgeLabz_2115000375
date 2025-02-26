7cf4dc89f4055a8cd664e950bd1e5d1ed631ccff
Divyasingh001 <divyashunk2003@gmail.com>
2025-02-26 19:13:01 +0530

    Added

---
+++ BL_Assignment_26-02-2025/StudentDetails.java
@@ -0,0 +1,20 @@
+package org.example;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        JSONObject student = new JSONObject();
+        student.put("name", "John Doe");
+        student.put("age", 20);
+
+        JSONArray subjects = new JSONArray();
+        subjects.put("Math");
+        subjects.put("Science");
+        subjects.put("History");
+
+        student.put("subjects", subjects);
+
+        System.out.println(student.toString(4));
+    }
+}
